ac76c59467d5f780f90e11eaf5159063da0afce8
ManonVillanueva <g.manon14@gmail.com>
2024-10-15 15:35:53 +0200

    add top&flop btn

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import defaultPoster from "../assets/img/poster.jpg";
 import axios from "axios";
 
-const Cards = ({ dataSend }) => {
+const Cards = ({ movie }) => {
   const urlImg = "https://image.tmdb.org/t/p/original/";
   const [dataGenre, setDataGenre] = useState([]);
 
@@ -17,34 +17,34 @@ const Cards = ({ dataSend }) => {
   return (
     <div>
       <ul>
-        {dataSend.map((movie) => (
-          <li key={movie.id}>
-            <img
-              src={
-                movie.backdrop_path
-                  ? `${urlImg}${movie.backdrop_path}`
-                  : defaultPoster
-              }
-              alt={`affiche de ${movie.title}`}
-            />
-            <h2>{movie.title}</h2>
-            {/* Pour inverser la date :
+        <li>
+          <img
+            src={
+              movie.backdrop_path
+                ? `${urlImg}${movie.backdrop_path}`
+                : defaultPoster
+            }
+            alt={`affiche de ${movie.title}`}
+          />
+          <h2>{movie.title}</h2>
+          {/* Pour inverser la date :
             1. .split("-"), qui divise la chaîne au niveau des tirets en créant un tableau
             2. .reverse() inverse l'ordre des éléments du tableau
             3. .join("-") reconstruit une chaîne de caractère en concaténant tous les éléments du tableau , séparés par des tirets */}
-            <p>
-              Sortie le : {movie.release_date.split("-").reverse().join("-")}
-            </p>
-            <p>{movie.vote_average}</p>
-            <p>
-              {dataGenre
-                .filter((category) => movie.genre_ids.includes(category.id))
-                .map((category) => category.name)
-                .join(" ")}
-            </p>
-            <p>{movie.overview}</p>
-          </li>
-        ))}
+          <p>Sorti le : {movie.release_date.split("-").reverse().join("-")}</p>
+          <p>
+            {Math.round(movie.vote_average * 10) / 10}{" "}
+            <i className="fa-solid fa-star"></i>
+          </p>
+          <p>
+            {dataGenre
+              .filter((category) => movie.genre_ids.includes(category.id))
+              .map((category) => category.name)
+              .join(" ")}
+          </p>
+          <p>{movie.overview}</p>
+          <button>Ajouter aux favoris</button>
+        </li>
       </ul>
     </div>
   );
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,13 +2,14 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Cards from "./Cards";
 
-const Movies = () => {
+const Navigation = () => {
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("code");
+  const [sortVote, setSortVote] = useState(null);
   useEffect(() => {
     axios
       .get(
-        `https://api.themoviedb.org/3/search/movie?query="${search}&api_key=cd100debd32790befdf1a810dcd36ddd&language=fr-FR`
+        `https://api.themoviedb.org/3/search/movie?query=${search}&api_key=cd100debd32790befdf1a810dcd36ddd&language=fr-FR`
       )
       .then((res) => setData(res.data.results));
   }, [search]);
@@ -22,10 +23,26 @@ const Movies = () => {
             e.target.value ? setSearch(e.target.value) : setSearch("code")
           }
         />
+        <button onClick={() => setSortVote("badToGood")}>
+          Top <i className="fa-solid fa-arrow-up"></i>
+        </button>
+        <button onClick={() => setSortVote("goodToBad")}>
+          Flop <i className="fa-solid fa-arrow-down"></i>
+        </button>
       </div>
-      <Cards dataSend={data} />
+      {data
+        .sort((a, b) => {
+          if (sortVote === "badToGood") {
+            return a.vote_average - b.vote_average;
+          } else if (sortVote === "goodToBad") {
+            return b.vote_average - a.vote_average;
+          }
+        })
+        .map((movie) => (
+          <Cards key={movie.id} movie={movie} />
+        ))}
     </div>
   );
 };
 
-export default Movies;
+export default Navigation;
